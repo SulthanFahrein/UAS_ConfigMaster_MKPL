5a29de7e0b5e8338392363cc97e6a0cb954775c6
Debi Ratnasari Novianti <debinv4nt@gmail.com>
2023-06-13 19:56:17 +0700

    Update SubtitleDownloader.java

--- src/main/java/SubtitleDownloader.java
+++ src/main/java/SubtitleDownloader.java
@@ -176,8 +176,9 @@ public class SubtitleDownloader {
             }
             flag = false;
         }
-        System.out.println("\nMovie: " + m
-        // HTML Content of all Movies  the movie name
+        System.out.println("\nMovie: " + movieName);
+
+        // HTML Content of all Movies matching the movie name
         Elements elements = getSearchResults(movieName);
 
         // If the result is empty, Ask the User to input the Movie name
@@ -451,6 +452,7 @@ public class SubtitleDownloader {
         String movieName = element.select("h3.media-heading").text();
         String year = element.select("span.movinfo-section").first().text().substring(0, 4);
         System.out.println(n + ". " + movieName + " " + year);
+        System.out.println(n + ". " + movieName + " "  );
 
         
     }
